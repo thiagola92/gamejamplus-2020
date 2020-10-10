d1b4cab9bf00e5c7d9576e45b038ad9a1cf85e51
thiagola92 <thiagola92@gmail.com>
2020-10-09 23:59:10 -0300

    Base player

---
+++ Game/scripts/Player.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+		if Input.is_key_pressed(KEY_A):
+			position.x -= 150 * delta
+		elif Input.is_key_pressed(KEY_D):
+			position.x += 150 * delta
