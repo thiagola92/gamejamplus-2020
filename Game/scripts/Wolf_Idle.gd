74cd7dcf85ff6484e2bb2d89073d3dfbc1df612c
Stephanie Fay <stephaniemfay@gmail.com>
2020-10-11 16:38:02 -0300

    credits scene

--- Game/scripts/Wolf_Idle.gd
+++ Game/scripts/Wolf_Idle.gd
@@ -12,7 +12,7 @@ func _ready():
 	pass # Replace with function body.
 	
 func _process(delta):
-	if Input.is_key_pressed(KEY_SPACE):
+	if Input.is_action_just_pressed("ui_accept"):
 		$AnimatedSprite.play("gb")
 		get_parent().get_node("Label").hide()
 		get_parent().get_node("Label2").hide()
