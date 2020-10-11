2abcccf95589430b2d3db9830ec3a888da219a7c
Stephanie Fay <stephaniemfay@gmail.com>
2020-10-11 12:18:03 -0300

    Initial Screen

---
+++ Game/scripts/Wolf_Idle.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+var next = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+func _process(delta):
+	if Input.is_key_pressed(KEY_SPACE):
+		$AnimatedSprite.play("gb")
+		get_parent().get_node("Label").hide()
+		get_parent().get_node("Label2").hide()
+		next = true
+		move_and_slide(Vector2(-2200, 380))
+
+
+func _on_AnimatedSprite_animation_finished():
+	if next:
+		$AnimatedSprite.set_frame(7)
+		$AnimatedSprite.stop()
+		get_tree().change_scene("res://scenes/Level 1.tscn")
